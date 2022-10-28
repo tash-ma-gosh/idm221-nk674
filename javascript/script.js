c2ade10df20515a21cc695b074b4b64108655846
tash-ma-gosh <n.karnoto@gmail.com>
2022-10-27 22:55:33 -0400

    tab content on experience

--- javascript/script.js
+++ javascript/script.js
@@ -7,3 +7,24 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
   }
+
+  function openJob(evt, jobName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(jobName).style.display = "block";
+    evt.currentTarget.className += " active";
+  } 
